c090cc8f6f125a4750326c08c73a2811e3bfb7be
Olga Murashova <om.dreyfus@gmail.com>
2022-09-24 16:46:11 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+
+const btn = document.querySelector(".btn");
+
+const popupContainer = document.querySelector(".popup-container");
+
+const closeIcon = document.querySelector(".close-icon");
+
+btn.addEventListener("click", ()=> {
+    container.classList.add("active");
+    popupContainer.classList.remove("active");
+
+});
+
+closeIcon.addEventListener("click", ()=> {
+    container.classList.remove("active");
+    popupContainer.classList.add("active");
+
+})
